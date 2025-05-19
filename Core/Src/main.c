79cd29401e082c32723899c651af12ac545e5eca
Beszti85 <csababesztercei@gmail.com>
2025-05-19 22:47:11 +0200

    [ADC]: added 5th element to ADC variables

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,8 +119,8 @@ const osEventFlagsAttr_t EventComTask_attributes = {
   .cb_size = sizeof(myEvent01ControlBlock),
 };
 /* USER CODE BEGIN PV */
-volatile uint16_t ADC_RawData[4u] = {0u};
-float ADC_Voltage[4u];
+volatile uint16_t ADC_RawData[5u] = {0u};
+float ADC_Voltage[5u];
 
 NRF24L01_Handler_t RFHandler =
 {
@@ -458,7 +458,7 @@ static void MX_ADC1_Init(void)
   hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
   hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.NbrOfConversion = 4;
+  hadc1.Init.NbrOfConversion = 5;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
@@ -506,6 +506,16 @@ static void MX_ADC1_Init(void)
   sConfig.Channel = ADC_CHANNEL_VREFINT;
   sConfig.Rank = ADC_REGULAR_RANK_4;
   sConfig.SamplingTime = ADC_SAMPLETIME_24CYCLES_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_REGULAR_RANK_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_47CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -763,11 +773,11 @@ void StartTask100ms(void *argument)
   for(;;)
   {
     // Convert ADC raw data from last running
-    for( uint16_t i = 0u; i < 4u; i++ )
+    for( uint16_t i = 0u; i < 5u; i++ )
     {
       ADC_Voltage[i] = (float)ADC_RawData[i] * 3.3f / 4096.0f;
     }
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&ADC_RawData[0u], 4u);
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&ADC_RawData[0u], 5u);
     osDelay(100);
   }
   /* USER CODE END 5 */
