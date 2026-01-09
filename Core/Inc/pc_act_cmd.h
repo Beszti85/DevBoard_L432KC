6a6c39783aec50459fa82a45c0e71536c35bce27
Beszti85 <csababesztercei@gmail.com>
2026-01-09 16:37:21 +0100

    [PC]: added flash read and write

--- Core/Inc/pc_act_cmd.h
+++ Core/Inc/pc_act_cmd.h
@@ -12,6 +12,9 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define PC_CMD_FLASH_READ    0x01u
+#define PC_CMD_FLASH_WRITE   0x02u
+
 void PC_ExecCmdHandler( uint8_t* ptrRxBuffer, uint8_t* ptrTxBuffer );
 
 #endif /* INC_PC_ACT_CMD_H_ */
--- Core/Src/pc_act_cmd.c
+++ Core/Src/pc_act_cmd.c
@@ -6,16 +6,35 @@
  */
 
 #include "pc_act_cmd.h"
+#include "flash.h"
+
+extern FLASH_Handler_t FlashHandler;
 
 void PC_ExecCmdHandler( uint8_t* ptrRxBuffer, uint8_t* ptrTxBuffer )
 {
   uint8_t cmd = ptrRxBuffer[0];
+  uint32_t tmpU32 = 0u;
+  uint16_t tmpU16 = 0u;
   // Cehck first byte
   switch (cmd)
   {
     // Read external flash
-    case 1:
-      //FLASH_Read()
+    case PC_CMD_FLASH_READ:
+      tmpU32 = ( (uint32_t)ptrRxBuffer[1] << 16u ) |
+               ( (uint32_t)ptrRxBuffer[2] << 8u )  |
+               ( (uint32_t)ptrRxBuffer[3] );
+      tmpU16 = ( (uint16_t)ptrRxBuffer[4] << 8u ) |
+               ( (uint16_t)ptrRxBuffer[5] );
+      FLASH_Read(&FlashHandler, tmpU32, ptrTxBuffer, tmpU16);
+      break;
+    // Write external flash
+    case PC_CMD_FLASH_WRITE:
+      tmpU32 = ( (uint32_t)ptrRxBuffer[1] << 16u ) |
+               ( (uint32_t)ptrRxBuffer[2] << 8u )  |
+               ( (uint32_t)ptrRxBuffer[3] );
+      tmpU16 = ( (uint16_t)ptrRxBuffer[4] << 8u ) |
+               ( (uint16_t)ptrRxBuffer[5] );
+      FLASH_Write(&FlashHandler, tmpU32, &ptrRxBuffer[6], tmpU16);
       break;
   }
 }
